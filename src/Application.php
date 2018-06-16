d1aa677f3e6a6bb9435bf5fcb542d8512100e848
aequabit <contact@aequabit.de>
2018-06-16 15:33:43 +0200

    Initial commit

---
+++ src/Application.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+class Application
+{
+    /**
+     * Runs the application.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        echo 'Hello World';
+    }
+}
